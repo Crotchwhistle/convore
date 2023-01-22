a60eef71786e6e996648bf03a5dd420d24ed8588
Suhaan <101968109+Crotchwhistle@users.noreply.github.com>
2023-01-22 18:31:58 +1100

    created a simple ping command to keep the bot going. now time to work on the ai.....

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -8,10 +9,18 @@ intents = discord.Intents.default()
 
 class myClient(discord.Client):
     async def on_ready(self):
+        await tree.sync(guild=discord.Object(id=964294311715938308))
         print(f"Logged in as {self.user}.")
 
     async def on_message(self, message):
         print(f"Message from {message.author}: {message.content}")
 
 client = myClient(intents=intents)
+tree = app_commands.CommandTree(client)
+
+# test ping command
+@tree.command(name="ping", description="Pings the bot.", guild=discord.Object("964294311715938308"))
+async def ping_command(interaction):
+    await interaction.response.send_message("Pong!")
+
 client.run(TOKEN)
